761f37a8c04892b5357bfa237114ca41f91dd23c
Maxotta1811 <maxiotta_15@gmail.com>
2023-05-06 22:11:04 -0300

    primer commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,18 @@
+import '../styles/App.css';
+import Header from "./Header"
+import Body from "./Body"
+import Footer from './Footer';
+
+function App() {
+  return (
+    
+    <>
+    <Header></Header>
+    <Body></Body>
+    <Footer></Footer>
+    </>
+
+  );
+}
+
+export default App;
